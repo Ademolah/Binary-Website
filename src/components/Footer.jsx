997a8c7efcc994d27869c0eab625db4acedf0e85
Charles <adeakinyemi129@gmail.com>
2025-07-21 23:52:06 +0100

    implemented footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = ()=>{
+    <footer className="bg-[#00477B] text-white text-center py-6 mt-16">
+        <p>&copy; {new Date().getFullYear()} Binary. All rights reserved.</p>
+    </footer>
+}
+
+export default Footer;
